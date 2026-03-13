0edda9bd144b929c35609cdb67f3a6de4bedc9c8
Juha Ritakoski <juha.ritakoski@hotmail.com>
2026-03-13 18:48:15 +0200

    add deletePersonalDefault command

---
+++ commands/deletePersonalDefault.js
@@ -0,0 +1,22 @@
+const fs = require('node:fs');
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('delete_personal_default')
+        .setDescription('Deletes your personal default so you use the global profile by default again.')
+        ,
+    async execute(interaction) {
+        const defaultProfiles = require("../data/default_profiles.json");
+        delete defaultProfiles[interaction.user.id];
+
+        fs.writeFile('data/default_profiles.json', JSON.stringify(defaultProfiles, null, 4), async (error) => {
+            if (error) console.error(error);
+        });
+
+        await interaction.reply({
+            content: `Your personal default bounties profile has been deleted.`,
+            flags: MessageFlags.Ephemeral
+        });
+    }
+}
